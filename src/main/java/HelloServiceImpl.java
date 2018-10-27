7e9fc76484e9c48ffaeb273d2a9233f2b9fc4996
chaialong <chaialong@gmail.com>
2018-10-27 10:54:24 +0800

    a simple rpc framework
    Signed-off-by: chaialong <chaialong@gmail.com>

---
+++ src/main/java/HelloServiceImpl.java
@@ -0,0 +1,5 @@
+public class HelloServiceImpl implements HelloService{
+    public String hello(String name) {
+        return "hello " + name;
+    }
+}
---
+++ src/main/java/RpcProvider.java
@@ -0,0 +1,10 @@
+public class RpcProvider {
+    public static void main(String[] args) {
+        try {
+            HelloService service = new HelloServiceImpl();
+            RpcFramework.export(service, 1235);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
